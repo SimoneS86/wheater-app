2158ddb5adeb12d3799034a9cca290fbd04ab405
SimoneS86 <simone.sensini.86@gmail.com>
2023-04-12 17:00:28 +0200

    step1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import MyHome from "./Components/MyHome";
+
+function App() {
+  return (
+    <MyHome />
+  );
+}
+
+export default App;
